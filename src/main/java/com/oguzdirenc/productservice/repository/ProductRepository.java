3d113dd8d97fbf9dfd85fcb3aee615648976f3ca
oguzdirenc <oguzdirenc@hotmail.com>
2023-09-16 21:02:51 +0300

    implemented test dependencies and created docker-mongoDB connection

--- src/main/java/com/oguzdirenc/productservice/repository/ProductRepository.java
+++ src/main/java/com/oguzdirenc/productservice/repository/ProductRepository.java
@@ -2,6 +2,8 @@ package com.oguzdirenc.productservice.repository;
 
 import com.oguzdirenc.productservice.model.Product;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ProductRepository extends MongoRepository<Product,String> {
 }
--- src/test/java/com/oguzdirenc/productservice/ProductServiceApplicationTests.java
+++ src/test/java/com/oguzdirenc/productservice/ProductServiceApplicationTests.java
@@ -2,10 +2,21 @@ package com.oguzdirenc.productservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.MongoDBContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
+@Testcontainers
 class ProductServiceApplicationTests {
-
+	@Container
+	static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:latest");
+	@DynamicPropertySource
+	static void setProperties(DynamicPropertyRegistry dymDynamicPropertyRegistry) {
+		dymDynamicPropertyRegistry.add("spring.data.mongodb.uri", mongoDBContainer::getReplicaSetUrl);
+	}
 	@Test
 	void contextLoads() {
 	}
